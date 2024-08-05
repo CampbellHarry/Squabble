7d2d780cb9342907cd1adbe7877ea5c2678eba37
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2024-08-05 23:16:04 +0100

    fixed issues with signup and login modal

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const showPopupBtn = document.querySelector(".login-btn");
 const hidePopupBtn = document.querySelector(".formPopup .close-btn");
 const formPopup = document.querySelector(".formPopup");
-const loginSignupLink = document.querySelectorAll(".formBox .signup a")
+const loginPopup = document.querySelector("#loginPopup");
+const signupPopup = document.querySelector("#signUpPopUp");
+const signupLink = document.querySelector("#signupLink");
+const loginLink = document.querySelector("#loginLink");
 
-//Show form popup
+// Show form popup
 showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
+    document.body.classList.add("show-popup");
+    loginPopup.style.display = "flex";
+    signupPopup.style.display = "none";
 });
 
-//Hide form popup
-hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+// Hide form popup
+hidePopupBtn.addEventListener("click", () => {
+    document.body.classList.remove("show-popup");
+});
 
-loginSignupLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === "signupLink" ? 'add' : 'remove']("show-signup");
-    });
+// Switch to signup form
+signupLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginPopup.style.display = "none";
+    signupPopup.style.display = "flex";
+});
+
+// Switch to login form
+loginLink.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    signupPopup.style.display = "none";
+    loginPopup.style.display = "flex";
 });
